58c57c5a7085ccb9f7e6b62e8b6e0c7167f7ae9f
Gbkng <guillaume.brooking@gmail.com>
2025-07-12 14:25:16 +0200

    feat: add a script to generate compile_commands.json

---
+++ generate_compcmd_json.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# ref: https://github.com/ziglang/zig/issues/9323#issuecomment-1646590552
+
+# - create a JSON array of JSON objects from .cache/cdb, and let a void object at
+# the end.
+# - populate void JSON object with ??? (seems brittle)
+# - format JSON with jq
+# - filter out lines with "no-default-cofig"
+
+(echo "["; cat .cache/cdb/*; echo "{}]") | 
+  perl -0777 -pe 's/,\n\{\}//igs' | 
+  jq . | 
+  grep -v 'no-default-config' > compile_commands.json

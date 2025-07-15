81dc46b4cee34e2339556be64fd149e28d33beaa
Gbkng <guillaume.brooking@gmail.com>
2025-07-15 13:26:41 +0200

    fix: format printf with new lines

--- src/bin/main.c
+++ src/bin/main.c
@@ -3,12 +3,12 @@
 #include <ncurses.h>
 
 int main(void) {
-  printf("using foo...");
+  printf("using foo...\n");
   foo();
 
-  printf("using ncurses...");
+  printf("using ncurses...\n");
   initscr();
-  printw("Press a key...");
+  printw("Press a key...\n");
   refresh();
   getch();
   endwin();

3230e607e1be2ee7e580077fd5982720e6b33c4b
Gbkng <guillaume.brooking@gmail.com>
2025-07-19 12:50:45 +0200

    feat: use MPI in main.c

--- src/bin/main.c
+++ src/bin/main.c
@@ -3,7 +3,23 @@
 #include <mpi.h>
 #include <ncurses.h>
 
-int main(void) {
+int main(int argc, char **argv) {
+
+  {
+    printf("initializing MPI...\n");
+    MPI_Init(&argc, &argv);
+
+    int rank;
+    int size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    printf("#%d/%d finalizaing MPI...\n", rank, size-1);
+
+    MPI_Finalize();
+  }
+
+  // foo is a local project library
   printf("using foo...\n");
   foo();
 
